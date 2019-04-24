c7902d0bbc177d2f8536d57a72181274df4a95e0
Bahman Movaqar <bahman@bahmanm.com>
2019-04-24 15:57:45 +0300

    handle top-level compose options

--- index.js
+++ index.js
@@ -2,98 +2,100 @@
 
 const { spawn } = require('child_process');
 const debug = require('debug')('docker-compose-js');
-const Promise = require('bluebird');
 
-module.exports = function compose(composeFile) {
-    function run(command, options = {}, services, param1) {
-        return new Promise(((resolve, reject) => {
-            let stdout = '';
-            let stderr = '';
+module.exports = function compose(composeFile, composeOptions = {}) {
+  function objectToOptions(obj) {
+    const result = [];
+    Object.keys(obj).forEach(k => {
+      if (k.length <= 2) {
+        if (k.indexOf('-') === 0) {
+          result.push(k);
+        } else {
+          result.push(`-${k}`);
+        }
+        if (obj[k])
+          result.push(obj[k]);
+      } else {
+        const optName = (k.indexOf('--') === 0) ? k : `--${k}`;
+        const optValue = obj[k] ? `=${obj[k]}` : '';
+        const optStr = `${optName}${optValue}`;
+        result.push(optStr);
+      }
+    });
+    return result;
+  }
 
-            let args = ['-f', composeFile, command];
+  function run(command, options = {}, services, param1) {
+    return new Promise(
+      (function(resolve, reject) {
+        let stdout = '';
+        let stderr = '';
+        let args = [
+          '-f', composeFile,
+          ...objectToOptions(composeOptions),
+          command,
+          ...objectToOptions(options)
+        ];
+        if (services) {
+          if (Array.isArray(services)) {
+            args = args.concat(services);
+          } else {
+            args.push(services);
+          }
+        }
 
-            // parse the options and append the -- or - if missing
-            Object.keys(options).forEach((option) => {
-                if (option.length <= 2) {
-                    if (option.indexOf('-') === 0) {
-                        args.push(option);
-                    } else {
-                        args.push(`-${option}`);
-                    }
+        // Some commands support an additional parameter
+        if (param1) args.push(param1);
+        debug('docker-compose', args);
+        const cmd = spawn('docker-compose', args);
 
-                    // not all options have attached values.
-                    if (options[option]) args.push(options[option]);
-                    return;
-                }
+        cmd.stdout.on('data', (data) => {
+          debug('stdout', data.toString());
+          stdout += data;
+        });
 
-                let param = `--${option}`;
-                if (option.indexOf('--') === 0) {
-                    param = option;
-                }
+        cmd.stderr.on('data', (data) => {
+          debug('stderr', data.toString());
+          stderr += data;
+        });
 
-                if (options[option]) param += `=${options[option]}`;
-                args.push(param);
-            });
+        cmd.on('close', (code) => {
+          debug('close with code', code);
+          if (code !== 0) {
+            reject(`Command exited: ${code}\n${stderr}`);
+          } else {
+            resolve(stdout);
+          }
+        });
+      })
+    );
+  }
 
-            if (services) {
-                if (Array.isArray(services)) {
-                    args = args.concat(services);
-                } else {
-                    args.push(services);
-                }
-            }
-
-            // Some commands support an additional parameter
-            if (param1) args.push(param1);
-            debug('docker-compose', args);
-            const cmd = spawn('docker-compose', args);
-
-            cmd.stdout.on('data', (data) => {
-                debug('stdout', data.toString());
-                stdout += data;
-            });
-
-            cmd.stderr.on('data', (data) => {
-                debug('stderr', data.toString());
-                stderr += data;
-            });
-
-            cmd.on('close', (code) => {
-                debug('close with code', code);
-                if (code !== 0) {
-                    reject(`Command exited: ${code}\n${stderr}`);
-                } else {
-                    resolve(stdout);
-                }
-            });
-        }));
-    }
-
-    return {
-        up: (options = {}) => {
-            options.d = '';
-            return run('up', options);
-        },
-        down: options => run('down', options),
-        ps: options => run('ps', options),
-        start: (services, options) => run('start', options, services),
-        stop: (services, options) => run('stop', options, services),
-        restart: (services, options) => run('restart', options, services),
-        kill: (services, options) => run('kill', options, services),
-        pull: (services, options) => run('pull', options, services),
-        create: (services, options) => run('create', options, services),
-        version: options => run('version', options),
-        pause: (services, options) => run('pause', options, services),
-        unpause: (services, options) => run('unpause', options, services),
-        scale: (services, options) => run('scale', options, services),
-        rm: (services, options) => run('rm', options, services),
-        // logs is going to require special handling since it attaches to containers
-        // logs: (services, options) => { return run('logs', options, services); },
-        port: (service, privatePort, options) => run('port', options, service, privatePort),
-        run,
-        /*
-            Currently unimplemented
-                events
-        */
-    };
+  return {
+    up: (options = {}) => {
+      options.d = '';
+      return run('up', options);
+    },
+    down: options => run('down', options),
+    ps: options => run('ps', options),
+    start: (services, options) => run('start', options, services),
+    stop: (services, options) => run('stop', options, services),
+    restart: (services, options) => run('restart', options, services),
+    kill: (services, options) => run('kill', options, services),
+    pull: (services, options) => run('pull', options, services),
+    create: (services, options) => run('create', options, services),
+    version: options => run('version', options),
+    pause: (services, options) => run('pause', options, services),
+    unpause: (services, options) => run('unpause', options, services),
+    scale: (services, options) => run('scale', options, services),
+    rm: (services, options) => run('rm', options, services),
+    // logs is going to require special handling since it attaches to containers
+    // logs: (services, options) => { return run('logs', options, services); },
+    port: (service, privatePort, options) => run('port', options, service, privatePort),
+    run,
+    /*
+      Currently unimplemented
+      events
+    */
+  };
 };
